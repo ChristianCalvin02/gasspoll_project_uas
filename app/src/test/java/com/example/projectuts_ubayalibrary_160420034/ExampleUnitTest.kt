c0633bcb7ed534e208bca58fa01aa4132508ee87
ChristianCalvin02 <christian.calvinwijaya02@gmail.com>
2023-04-26 19:25:59 +0700

    Project UTS Tahap 1

---
+++ app/src/test/java/com/example/projectuts_ubayalibrary_160420034/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.projectuts_ubayalibrary_160420034
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
