163b8be933829b00964970b05de6efceca53bda3
ChristianCalvin02 <christian.calvinwijaya02@gmail.com>
2023-05-03 22:06:51 +0700

    Project UTS tahap 2 code

--- app/src/main/java/com/example/projectuts_ubayalibrary_160420034/util/util.kt
+++ app/src/main/java/com/example/projectuts_ubayalibrary_160420034/util/util.kt
@@ -1,2 +1,20 @@
 package com.example.projectuts_ubayalibrary_160420034.util
 
+import android.app.NotificationChannel
+import android.content.Context
+import android.os.Build
+import android.widget.ImageView
+import android.widget.ProgressBar
+
+fun createNotifChannel(context: Context, importance: Int, showBagde: Boolean, name: String, desc:String){
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        val channelID = "${context.packageName}-$name"
+        val channel = NotificationChannel(channelID, name, importance)
+        channel.description = desc
+        channel.setShowBadge(showBagde)
+    }
+}
+
+fun ImageView.loadImage(url:String?, progressBar: ProgressBar){
+
+}
