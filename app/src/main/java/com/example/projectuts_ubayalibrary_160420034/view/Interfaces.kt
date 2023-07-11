d6aacb2ab4e57af88c08769b7af9a899a93ba8f8
ChristianCalvin02 <christian.calvinwijaya02@gmail.com>
2023-07-11 14:39:53 +0700

    Project UAS Tahap 1 Data Binding (Christian Calvin Wijaya - 160420034)

---
+++ app/src/main/java/com/example/projectuts_ubayalibrary_160420034/view/Interfaces.kt
@@ -0,0 +1,7 @@
+package com.example.projectuts_ubayalibrary_160420034.view
+
+import android.view.View
+
+interface ButtonDetailClickListener{
+    fun onButtonDetailClick(v:View)
+}
--- app/src/main/java/com/example/projectuts_ubayalibrary_160420034/view/LibraryListAdapter.kt
+++ app/src/main/java/com/example/projectuts_ubayalibrary_160420034/view/LibraryListAdapter.kt
@@ -7,19 +7,25 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projectuts_ubayalibrary_160420034.R
+import com.example.projectuts_ubayalibrary_160420034.databinding.LibraryListItemBinding
 import com.example.projectuts_ubayalibrary_160420034.model.Library
 import com.example.projectuts_ubayalibrary_160420034.util.loadImage
+import com.example.projectuts_ubayalibrary_160420034.viewmodel.DetailViewModel
+import com.example.projectuts_ubayalibrary_160420034.viewmodel.RecommendDetailViewModel
 
 class LibraryListAdapter(val libraryList:ArrayList<Library>):
-    RecyclerView.Adapter<LibraryListAdapter.LibraryViewHolder>(){
-    class LibraryViewHolder(var view: View):RecyclerView.ViewHolder(view)
+    RecyclerView.Adapter<LibraryListAdapter.LibraryViewHolder>()
+    , ButtonDetailClickListener {
+    class LibraryViewHolder(var view: LibraryListItemBinding):RecyclerView.ViewHolder(view.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):LibraryViewHolder{
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.library_list_item, parent, false)
+        val view = DataBindingUtil.inflate<LibraryListItemBinding>(inflater,
+            R.layout.library_list_item, parent, false)
         return LibraryViewHolder(view)
     }
 
@@ -28,26 +34,33 @@ class LibraryListAdapter(val libraryList:ArrayList<Library>):
     }
 
     override fun onBindViewHolder(holder: LibraryViewHolder, position: Int) {
-        val txtId = holder.view.findViewById<TextView>(R.id.txtIdBook)
+        holder.view.library =libraryList[position]
+        holder.view.listener = this
+        /*val txtId = holder.view.findViewById<TextView>(R.id.txtIdBook)
         val txtName = holder.view.findViewById<TextView>(R.id.txtBookName)
         val btnDetail = holder.view.findViewById<Button>(R.id.btnDetail)
         txtId.text = libraryList[position].id
-        txtName.text = libraryList[position].book_name
-        btnDetail.setOnClickListener {
+        txtName.text = libraryList[position].book_name*/
+        /*btnDetail.setOnClickListener {
             val id = txtId.text.toString()
             val action = LibraryListFragmentDirections.actionLibraryDetail(id)
             Navigation.findNavController(it).navigate(action)
-        }
+        }*/
 
-        var imageView = holder.view.findViewById<ImageView>(R.id.imageViewLibrary)
+        /*var imageView = holder.view.findViewById<ImageView>(R.id.imageViewLibrary)
         var progressBar = holder.view.findViewById<ProgressBar>(R.id.progressBar)
-        imageView.loadImage(libraryList[position].image_url, progressBar!!)
+        imageView.loadImage(libraryList[position].image_url, progressBar!!)*/
     }
 
     fun updateLibraryList(newLibraryList: ArrayList<Library>){
         libraryList.clear()
         libraryList.addAll(newLibraryList)
         notifyDataSetChanged()
+    }
+
+    override fun onButtonDetailClick(v: View) {
+        val action = LibraryListFragmentDirections.actionLibraryDetail(v.tag.toString())
+        Navigation.findNavController(v).navigate(action)
     }
 }
 
