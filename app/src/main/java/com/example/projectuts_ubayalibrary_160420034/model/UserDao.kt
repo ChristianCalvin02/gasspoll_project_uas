4ba82250e50b1f107e51a9f47a3eb41e80537763
ChristianCalvin02 <christian.calvinwijaya02@gmail.com>
2023-07-18 13:30:30 +0700

    Project UAS Tahap 2 Data Binding Update Library(Jeremy Kenneth Gunawan - 160420020)

--- app/src/main/java/com/example/projectuts_ubayalibrary_160420034/model/UserDao.kt
+++ app/src/main/java/com/example/projectuts_ubayalibrary_160420034/model/UserDao.kt
@@ -3,9 +3,13 @@ package com.example.projectuts_ubayalibrary_160420034.model
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface UserDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg user: User)
+
+    @Query("SELECT user_name=:user_name && password=:password FROM user")
+    fun selectUser(user_name:String, password:String)
 }
--- app/src/main/java/com/example/projectuts_ubayalibrary_160420034/viewmodel/userViewModel.kt
+++ app/src/main/java/com/example/projectuts_ubayalibrary_160420034/viewmodel/userViewModel.kt
@@ -9,22 +9,36 @@ import com.android.volley.RequestQueue
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.projectuts_ubayalibrary_160420034.model.User
+import com.example.projectuts_ubayalibrary_160420034.util.buildDB2
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class userViewModel(application: Application):AndroidViewModel(application) {
+class userViewModel(application: Application):AndroidViewModel(application), CoroutineScope {
     val userLD = MutableLiveData<User>()
     val userLoadErrorLD = MutableLiveData<Boolean>()
     val loadingLD = MutableLiveData<Boolean>()
+    private val job = Job()
 
-    val TAG ="volleyTag"
-    private var queue: RequestQueue? = null
 
-    fun show(id: String){
+    /*val TAG ="volleyTag"
+    private var queue: RequestQueue? = null*/
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.IO
+
+    fun show(user_name: String, password: String){
         loadingLD.value = true
         userLoadErrorLD.value = false
 
-        queue = Volley.newRequestQueue(getApplication())
+        launch {
+            val db = buildDB2(getApplication())
+            db.userDao().selectUser(user_name, password)
+        }
+        /*queue = Volley.newRequestQueue(getApplication())
         val url = "https://project333401.000webhostapp.com/anmp_project/ubayaLibraryUser.php?id=$id"
 
         val stringRequest = StringRequest(
@@ -43,6 +57,6 @@ class userViewModel(application: Application):AndroidViewModel(application) {
         )
 
         stringRequest.tag = TAG
-        queue?.add(stringRequest)
+        queue?.add(stringRequest)*/
     }
 }
