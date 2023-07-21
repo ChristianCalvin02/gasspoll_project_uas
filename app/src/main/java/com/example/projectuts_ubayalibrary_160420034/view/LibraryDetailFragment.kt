9966b93936df5b026c60be8d106e876975a1d0f9
ChristianCalvin02 <christian.calvinwijaya02@gmail.com>
2023-07-21 14:13:08 +0700

    Project UAS Tahap 2 Data Binding Update User(Jeremy Kenneth Gunawan - 160420020)

--- app/src/main/java/com/example/projectuts_ubayalibrary_160420034/view/LibraryDetailFragment.kt
+++ app/src/main/java/com/example/projectuts_ubayalibrary_160420034/view/LibraryDetailFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -44,6 +46,11 @@ class LibraryDetailFragment : Fragment() {
             val id = LibraryDetailFragmentArgs.fromBundle(requireArguments()).id
             viewModel.show(id)
         }*/
+        val btnSewa = view.findViewById<Button>(R.id.btnSewa)
+        btnSewa.setOnClickListener {
+
+        }
+
         ObserveViewModels()
     }
 
